6889009efa22c77334bfbe2c0611e29168cf0127
Jeff Wong <jeffmjwong@gmail.com>
2019-08-29 19:43:27 +1000

    Write first test for react-testing-library and jest-dom

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('shows "Hello world!" in main App component', () => {
+  const { queryByText } = render(<App />);
+
+  expect(queryByText("Hello world!")).toBeInTheDocument();
 });
